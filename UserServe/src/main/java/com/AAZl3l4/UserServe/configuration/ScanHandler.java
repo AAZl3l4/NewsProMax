45e7dd9d6d788bfdcee10f4f306ff97cbc0e63d8
AAZl3l4 <AAZl3l4@qq.com>
2025-08-19 12:01:22 +0800

    查缺补漏

--- UserServe/src/main/java/com/AAZl3l4/UserServe/configuration/ScanHandler.java
+++ UserServe/src/main/java/com/AAZl3l4/UserServe/configuration/ScanHandler.java
@@ -10,11 +10,13 @@ import me.chanjar.weixin.mp.api.WxMpMessageHandler;
 import me.chanjar.weixin.mp.api.WxMpService;
 import me.chanjar.weixin.mp.bean.message.WxMpXmlMessage;
 import me.chanjar.weixin.mp.bean.message.WxMpXmlOutMessage;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 
 import java.time.Duration;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 @Component // 注册为 Spring 组件
 @RequiredArgsConstructor // 自动生成构造器注入
@@ -22,6 +24,8 @@ public class ScanHandler implements WxMpMessageHandler {
     private final IUserService userService; // 操作用户表
     private final JwtUtil jwtUtil; // 解析 & 生成 JWT的bean
     private final RedisTemplate<String, String> redis; // 临时存储登录 token
+    @Value("${Jwt.expiration}")
+    private int jwtExpiration;
 
     @Override
     public WxMpXmlOutMessage handle(WxMpXmlMessage msg, Map<String, Object> ctx, WxMpService wx, WxSessionManager sm) {
@@ -47,7 +51,10 @@ public class ScanHandler implements WxMpMessageHandler {
         // 2. 如果是“登录”二维码，则直接查库发 JWT
         User user = userService.getOne(new QueryWrapper<User>().eq("wx_id", openId)); // 登录场景查用户
         if (user != null) { // 已绑定
-            String token = jwtUtil.create(user);// 生成 JWT         // 把 token 临时放到 redis 前端轮询拉取
+            String token = jwtUtil.create(user);// 生成 JWT
+            // 更新reids中的 token
+            redis.opsForValue().set("jwt:" + user.getId(), token,jwtExpiration * 60000, TimeUnit.MILLISECONDS);
+            // 把 token 临时放到 redis 前端轮询拉取
             redis.opsForValue().set("wx_login_token_" + scene, token, Duration.ofMinutes(5)); // 缓存 5 分钟
             return WxMpXmlOutMessage.TEXT()
                     .content("登录成功，即将返回网站")  //登录时公众号的提示信息
